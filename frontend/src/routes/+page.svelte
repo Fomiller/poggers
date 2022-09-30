1506b5197012abd469ccdcf107a30ba3547ece6b
Forrest Miller <forrestmillerj@gmail.com>
2022-09-29 19:55:29 -0500

    testing svelte layout and dasiyui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<input
+  type="text"
+  placeholder="Type here"
+  class="input input-bordered input-primary w-full max-w-xs"
+/>
+
+<div class="stats bg-primary text-primary-content">
+  <div class="stat">
+    <div class="stat-title">Account balance</div>
+    <div class="stat-value">$89,400</div>
+    <div class="stat-actions">
+      <button class="btn btn-sm btn-success">Add funds</button>
+    </div>
+  </div>
+
+  <div class="stat">
+    <div class="stat-title">Current balance</div>
+    <div class="stat-value">$89,400</div>
+    <div class="stat-actions">
+      <button class="btn btn-sm">Withdrawal</button>
+      <button class="btn btn-sm">deposit</button>
+    </div>
+  </div>
+</div>
---
+++ frontend/src/routes/about/+layout.svelte
@@ -0,0 +1,5 @@
+<span class="countdown font-mono text-6xl">
+  <span style="--value:99;" />
+</span>
+
+<slot />
